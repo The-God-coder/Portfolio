6c5a94ab4f3aac034edfb20ef7105595b0c04de7
procodesbro <aniket332@outlook.com>
2019-05-01 16:39:54 -0400

    Finished the whole Project

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,11 @@ class Blog(models.Model):
     Date = models.DateTimeField()
     Body = models.CharField(max_length=400)
     image = models.ImageField(upload_to='images/')
+    def summary(self):
+        return self.Body[:300]
+
+    def __str__(self):
+        return self.title
     
     
     
--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summery = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summery
